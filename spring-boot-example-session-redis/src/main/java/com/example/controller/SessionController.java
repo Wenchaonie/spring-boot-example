b3c2bae7944b53b56797d8e9bc5125d4399431b6
nbxun <1479005017@qq.com>
2016-12-11 21:52:29 +0800

    添加通过redis共享http-session的example

---
+++ spring-boot-example-session-redis/src/main/java/com/example/controller/SessionController.java
@@ -0,0 +1,24 @@
+package com.example.controller;
+
+import java.util.UUID;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/session")
+public class SessionController {
+
+	@RequestMapping("/uid")
+	String uid(HttpSession session) {
+		UUID uid = (UUID) session.getAttribute("uid");
+		if (uid == null) {
+			uid = UUID.randomUUID();
+		}
+		session.setAttribute("uid", uid);
+		return uid.toString();
+	}
+
+}
