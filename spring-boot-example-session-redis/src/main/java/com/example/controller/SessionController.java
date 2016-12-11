b88dfd8a341c72a036f3d197586f6f4f126617f9
nbxun <1479005017@qq.com>
2016-12-11 22:32:19 +0800

    更新请求的内容为sessionId

--- spring-boot-example-session-redis/src/main/java/com/example/controller/SessionController.java
+++ spring-boot-example-session-redis/src/main/java/com/example/controller/SessionController.java
@@ -1,7 +1,5 @@
 package com.example.controller;
 
-import java.util.UUID;
-
 import javax.servlet.http.HttpSession;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,14 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/session")
 public class SessionController {
 
-	@RequestMapping("/uid")
-	String uid(HttpSession session) {
-		UUID uid = (UUID) session.getAttribute("uid");
-		if (uid == null) {
-			uid = UUID.randomUUID();
-		}
-		session.setAttribute("uid", uid);
-		return uid.toString();
+	@RequestMapping("/sid")
+	public String sid(HttpSession session) {
+		return session.getId();
 	}
 
 }
