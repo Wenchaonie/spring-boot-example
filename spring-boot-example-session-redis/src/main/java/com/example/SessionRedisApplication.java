9ee856106c876782bbf756c088bb2c63d843deb8
nbxun <1479005017@qq.com>
2017-02-21 16:10:12 +0800

    更新spring-boot-example-session-redis版本

--- spring-boot-example-session-redis/src/main/java/com/example/SessionRedisApplication.java
+++ spring-boot-example-session-redis/src/main/java/com/example/SessionRedisApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SessionRedisApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SessionRedisApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SessionRedisApplication.class, args);
+    }
 
 }
--- spring-boot-example-session-redis/src/main/java/com/example/filter/AfterRedisSessionFilter.java
+++ spring-boot-example-session-redis/src/main/java/com/example/filter/AfterRedisSessionFilter.java
@@ -1,30 +1,28 @@
 package com.example.filter;
 
-import java.io.IOException;
+import org.springframework.core.annotation.Order;
+import org.springframework.session.web.http.SessionRepositoryFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import org.springframework.core.annotation.Order;
-import org.springframework.session.web.http.SessionRepositoryFilter;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
+import java.io.IOException;
 
 // @Component
 @Order(SessionRepositoryFilter.DEFAULT_ORDER + 10)
 public class AfterRedisSessionFilter extends OncePerRequestFilter {
 
-	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-		HttpSession session = request.getSession();
-		System.out.println("after redis session: ");
-		System.out.println("request: " + request);
-		System.out.println("response: " + response);
-		System.out.println("session: " + session);
-		filterChain.doFilter(request, response);
-	}
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        System.out.println("after redis session: ");
+        System.out.println("request: " + request);
+        System.out.println("response: " + response);
+        System.out.println("session: " + session);
+        filterChain.doFilter(request, response);
+    }
 
 }
